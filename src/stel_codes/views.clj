7a5d293e9a351ae23616882b818beb361f000af9
Stel Abrego <stel@stel.codes>
2020-11-24 19:15:25 -0500

    got the nav up

--- src/stel_codes/views.clj
+++ src/stel_codes/views.clj
@@ -1,6 +1,6 @@
 (ns stel-codes.views
   (:require [hiccup.page :as hp]
-            [hiccup2.core :refer [html]]
+            [hiccup2.core :refer [html raw]]
             [hiccup.element :as he]
             [hiccup.form :as hf]))
 
@@ -12,6 +12,7 @@
           [:meta {:charset "utf-8"}]
           [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
           [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+          (hp/include-css "/assets/main.css")
           ; [:link {:rel "shortcut icon" :href "/assets/favicon.ico" :type "image/x-icon"}]
           ; (hp/include-css "/assets/minireset.css")
           ; (hp/include-css "/assets/main.css")
@@ -25,7 +26,24 @@
          [:body content])
    (str)))
 
+(defn header []
+[:header
+  [:nav
+   [:a#logoLink
+    {:href "/"} (raw (slurp "resources/svg/code.svg"))]
+   [:a#navName {:href "/"} "Stel Abrego"]
+   [:div#navBar.navBar-closed
+    [:ul.nav-ul-closed
+     [:li [:a {:href "/portfolio"} "Portfolio"]]
+     [:li [:a {:href "/resume"} "Résumé"]]
+     [:li [:a {:href "/tutoring"} "Tutoring"]]
+     [:li [:a {:href "/activism"} "Activism"]]
+     [:li [:a {:href "mailto:stelabrego@icloud.com"} "Contact"]]]]]]
+  )
+
+
+
 (defn home-page [context]
   (layout "stel.codes"
-          [:h1 "suppy"]
-          (he/image "/assets/background.jpg")))
+         (header) 
+          ))
