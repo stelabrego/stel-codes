5f2b40e6c3e47bc78c88808dad02be467082b6e5
Stel Abrego <stel@stel.codes>
2021-05-25 20:21:57 -0400

    add webhook functionality

---
+++ src/stelcodes/dev_blog/webhook.clj
@@ -0,0 +1,18 @@
+(ns stelcodes.dev-blog.webhook
+  (:require [org.httpkit.server :refer [run-server]]
+            [taoensso.timbre :as timbre :refer [info]]
+            [stelcodes.dev-blog.generator :refer [export]]))
+
+(def port 1201)
+
+(defn app
+  [_]
+  (info "Webhook request received. Initiating build...")
+  (export)
+  {:status 200})
+
+(defn listen
+  ([]
+   (info (str "Listening for webhooks on port " port))
+   (run-server app {:port port}))
+  ([_] (listen)))
