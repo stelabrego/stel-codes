c9351e99bfeb812a94526e73094a81bf00e9d6a1
Stel Abrego <stel@stel.codes>
2022-02-13 19:20:33 -0800

    Refactor generator to use new state functions

--- src/codes/stel/dev_blog/generator.clj
+++ src/codes/stel/dev_blog/generator.clj
@@ -1,25 +1,25 @@
 (ns codes.stel.dev-blog.generator
   (:require [stasis.core :as stasis]
-            [codes.stel.dev-blog.views :as views]
+            ;; [codes.stel.dev-blog.views :as views]
             [codes.stel.dev-blog.state :as state]
-            [clojure.java.io :refer [resource file]]
+            [clojure.java.io :as io]
             [me.raynes.fs :refer [copy-dir]]
             [taoensso.timbre :as timbre :refer [info]]))
 
 (defn generate-index
-  [pages]
-  (let [grouped-pages (group-by :type pages)]
-    (->> pages
-         (map (fn [page] {(:uri page) (fn [_] (views/render page grouped-pages))}))
-         (into {}))))
+  ([] (generate-index (state/realize-prod-site)))
+  ([{:keys [articles tag-indicies category-indicies] :as realized-site}]
+   (->> [{:uri "/" :category :home} {:uri "/404" :category :404}]
+        (concat articles tag-indicies category-indicies)
+        ;; TODO change back to views/render
+        (map #(vector (:uri %) (fn [_] (println % realized-site))))
+        (into {}))))
 
-(comment
-  (stasis/slurp-resources "public" #"")
-  (generate-index (state/get-published-pages)))
+(comment (generate-index))
 
 (defn generic-export
   ([site-index target-dir]
-   (let [assets (file (resource "public/assets"))]
+   (let [assets (io/file (io/resource "public/assets"))]
      (info "Building site...")
      (stasis/empty-directory! target-dir)
      (stasis/export-pages site-index target-dir)
@@ -27,13 +27,17 @@
      (info "Build successful")
      (info (str "Located in " target-dir)))))
 
-(defn export-published [] (generic-export (generate-index (state/get-published-pages)) "/www/dev-blog-published"))
+(defn export-prod []
+  (generic-export (generate-index (state/realize-prod-site)) "/dist/dev"))
 
-(defn export-preview [] (generic-export (generate-index (state/get-preview-pages)) "/www/dev-blog-preview"))
+(defn export-dev []
+  (generic-export (generate-index (state/realize-dev-site)) "/dist/prod"))
 
-(defn export ([] (export-published) (export-preview)) ([_] (export)))
+(defn export
+  ([] (export-prod) (export-dev))
+  ([_] (export)))
 
-(comment
-  (export)
-  (export-published)
-  (export-preview))
+;; (comment
+;;   (export)
+;;   (export-published)
+;;   (export-preview))
