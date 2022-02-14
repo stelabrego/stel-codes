281e809492499da8a2d6c1736f8d42b9811375bf
Stel Abrego <stel@stel.codes>
2022-02-13 19:51:02 -0800

    Comment out all of dev ns for now

--- dev/user.clj
+++ dev/user.clj
@@ -1,24 +1,26 @@
 (ns user
-  (:require [clojure.repl :refer [doc find-doc apropos source]]
-            [ring.middleware.resource :refer [wrap-resource]]
-            [stasis.core :as stasis]
-            [ring.middleware.content-type :refer [wrap-content-type]]
-            [codes.stel.dev-blog.generator :as generator]
-            [codes.stel.dev-blog.state :as state]
-            [org.httpkit.server :refer [run-server]]
-            [taoensso.timbre :as timbre :refer [info]]
-            [codes.stel.dev-blog.config :refer [config]]))
+  ;; (:require [clojure.repl :refer [doc find-doc apropos source]]
+  ;;           [ring.middleware.resource :refer [wrap-resource]]
+  ;;           [stasis.core :as stasis]
+  ;;           [ring.middleware.content-type :refer [wrap-content-type]]
+  ;;           [codes.stel.dev-blog.generator :as generator]
+  ;;           [codes.stel.dev-blog.state :as state]
+  ;;           [org.httpkit.server :refer [run-server]]
+  ;;           [taoensso.timbre :as timbre :refer [info]]
+  ;;           [codes.stel.dev-blog.config :refer [config]])
 
-(defn dev-app
-  []
-  (-> (stasis/serve-pages #(generator/generate-index (state/get-preview-pages)))
-      (wrap-resource "public")
-      wrap-content-type))
+  )
 
-(defn start
-  []
-  (let [dev-config (:dev config)
-        dev-port (:port dev-config)]
-    (info "Starting dev server on port " dev-port)
-    (run-server (dev-app) dev-config)))
-
+;; (defn dev-app
+;;   []
+;;   (-> (stasis/serve-pages #(generator/generate-index (state/get-preview-pages)))
+;;       (wrap-resource "public")
+;;       wrap-content-type))
+;;
+;; (defn start
+;;   []
+;;   (let [dev-config (:dev config)
+;;         dev-port (:port dev-config)]
+;;     (info "Starting dev server on port " dev-port)
+;;     (run-server (dev-app) dev-config)))
+;;
