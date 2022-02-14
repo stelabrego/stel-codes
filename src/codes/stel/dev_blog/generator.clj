2fa801833a50f839c29dce3007c56f5335f4c307
Stel Abrego <stel@stel.codes>
2022-02-13 23:48:03 -0800

    Refactor state into generator

--- src/codes/stel/dev_blog/generator.clj
+++ src/codes/stel/dev_blog/generator.clj
@@ -1,26 +1,109 @@
 (ns codes.stel.dev-blog.generator
   (:require [stasis.core :as stasis]
-            ;; [codes.stel.dev-blog.views :as views]
-            [codes.stel.dev-blog.state :as state]
+            [codes.stel.dev-blog.views :as views]
+            [codes.stel.dev-blog.util :as util]
             [clojure.java.io :as io]
             [me.raynes.fs :refer [copy-dir]]
-            [taoensso.timbre :as timbre :refer [info]]))
+            [taoensso.timbre :as timbre :refer [info]]
+            [codes.stel.dev-blog.config :refer [get-config]]
+            [markdown.core :refer [md-to-html-string]]))
+
+(defn create-tag-indices
+  ([] (create-tag-indices (get-config)))
+  ([{:keys [articles] :as _config}]
+  (let [tags (->> articles
+                  (mapcat :tags)
+                  (set))]
+    (for [tag tags]
+      (let [article-has-tag? (fn [article] (some #{tag} (:tags article)))]
+        {:title (str "#" (name tag)),
+         :id tag
+         :category :index
+         :uri (str "/tags/" (name tag) "/"),
+         :indexed-articles (->>
+                            articles
+                            (filter article-has-tag?)
+                            (map :id))})))))
+
+(defn create-category-indices
+  ([] (create-category-indices (get-config)))
+  ([{:keys [articles] :as _config}]
+  (let [categories (->> articles
+                        (map :category)
+                        (set))]
+    (for [category categories]
+      {:title (util/kebab-case->title-case category),
+       :id category
+       :category :index
+       :uri (str "/" (name category) "/"),
+       :indexed-articles (->>
+                          articles
+                          (filter #(= category (:category %)))
+                          (map :id))}))))
+
+(defn realize-articles
+  "Adds :uri and :render-markdown keys to each article map in articles vector"
+  [articles]
+  (for [{:keys [category resource-path id] :as article} articles]
+    (assoc article
+           :uri (str "/" (name category) "/" (name id) "/")
+           ;; I could render right here or I could just add a render function.
+           ;; Having a render function makes it much easier to inspect the full
+           ;; map output of this function in the repl
+           :render-resource (fn render-markdown []
+                              (if-let [resource (io/resource resource-path)]
+                                ;; TODO add support for other types of resources
+                                ;; based on filename suffix
+                                (md-to-html-string (slurp resource))
+                                (throw (ex-info "Missing resource" {:article article})))))))
+
+(defn realize-world
+  "Creates fully realized site datastructure with or without drafts."
+  ([] (realize-world (get-config) true))
+  ([include-drafts?] (realize-world (get-config) include-drafts?))
+  ([config include-drafts?]
+   ;; I'm using as-> here because the draft removal step affects the indicies.
+   ;; I could just redefine config in a let binding instead.
+    (as-> config $
+      ;; Remove drafts first if necessary
+       (if include-drafts? $ (update $ :articles #(remove :draft? %)))
+       ;; Realize the articles
+       (update $ :articles realize-articles)
+       ;; Add tag indicies
+       (assoc $ :tag-indicies (create-tag-indices $))
+       ;; Add category indicies
+       (assoc $ :category-indicies (create-category-indices $)))))
+
+(comment (realize-world))
+
+(defn id->info [{:keys [articles tag-indicies category-indicies]} id]
+  (->> (concat articles tag-indicies category-indicies)
+       (filter #(= id (:id %)))
+       (first)))
+
+(comment
+ (concat [1 2 3])
+ (id->info {:articles [{:id :poop :uri "/poop"}]} :poop))
 
 (defn generate-index
-  ([] (generate-index (state/realize-site)))
-  ([{:keys [articles tag-indicies category-indicies] :as realized-site}]
-   (->> [{:uri "/" :category :home} {:uri "/404" :category :404}]
+  ([] (generate-index (realize-world)))
+  ([{:keys [articles tag-indicies category-indicies] :as world}]
+   (->> [{:uri "/" :category :home} {:uri "/404/" :category :404}]
         (concat articles tag-indicies category-indicies)
-        ;; TODO change back to views/render
-        (map #(vector (:uri %) (fn [_] (println % realized-site))))
+        ;; Add id->info lookup function to every page
+        (map #(assoc % :id->info (partial id->info world)))
+        (map #(vector (:uri %) (fn [_] (views/render (assoc % :world world)))))
         (into {}))))
 
-(comment (generate-index))
+(comment (doseq [[n f] (generate-index)]
+           (println "rendering" n)
+           (println (f nil))))
 
 (defn generic-export
   ([site-index target-dir]
    (let [assets (io/file (io/resource "public/assets"))]
      (info "Building site...")
+     (fs)
      (stasis/empty-directory! target-dir)
      (stasis/export-pages site-index target-dir)
      (copy-dir assets target-dir)
@@ -28,16 +111,14 @@
      (info (str "Located in " target-dir)))))
 
 (defn export-prod []
-  (generic-export (generate-index (state/realize-site)) "/dist/dev"))
+  (generic-export (generate-index (realize-world)) "/dist/dev"))
 
 (defn export-dev []
-  (generic-export (generate-index (state/realize-dev-site)) "/dist/prod"))
+  (generic-export (generate-index (realize-world)) "/dist/prod"))
 
 (defn export
   ([] (export-prod) (export-dev))
   ([_] (export)))
 
-;; (comment
-;;   (export)
-;;   (export-published)
-;;   (export-preview))
+(comment
+  (export-dev))
--- src/codes/stel/dev_blog/views.clj
+++ src/codes/stel/dev_blog/views.clj
@@ -2,7 +2,7 @@
   (:require [hiccup2.core :refer [html raw]]
             [clojure.java.io :as io]
             [codes.stel.dev-blog.config :refer [config]]
-            [codes.stel.dev-blog.state :as state]))
+            [codes.stel.dev-blog.util :as util]))
 
 (defn image
   "Two-arity version is ambigious"
@@ -20,8 +20,8 @@
    [:ul opts (for [item items] [:li item])]))
 
 (defn header
-  [{:keys [general]}]
-  (let [{:keys [twitter github email]} general]
+  [{:keys [world]}]
+  (let [{:keys [twitter github email]} (:general world)]
     [:header
      [:nav [:a {:id "brand" :href "/"} (image "https://s3.stel.codes/nixos-logo.png") [:span "stel.codes"]]
       [:ul {:id "social"}
@@ -44,13 +44,13 @@
              (list "#" [:a {:class "tag" :href (-> (id->info tag) :uri)} tag]))])
 
 (defn window-list-item
-  [realized-site {:keys [uri title subtitle tags] :as _article}]
+  [{:keys [uri title subtitle tags] :as page}]
   (list [:a {:class "title" :href uri} title]
         (when subtitle [:p.subtitle subtitle])
-        (when (not-empty tags) (tag-group realized-site tags))))
+        (when (not-empty tags) (tag-group page tags))))
 
 (defn home-content-window
-  [{:keys [id->info] :as realized-site} category-id]
+  [{:keys [id->info] :as page} category-id]
   (let [{:keys [indexed-articles title uri]} (id->info category-id)]
     (when-not (empty? indexed-articles)
       (window title
@@ -59,7 +59,7 @@
                          (sort-by :sort)
                          (reverse)
                          (take 5)
-                         (map (partial window-list-item realized-site))
+                         (map window-list-item)
                          (unordered-list))
                     (when (> (count indexed-articles) 5) [:a {:class "more-link" :href uri} "more!"]))))))
 
@@ -73,7 +73,7 @@
    [:p "If you're interested in hiring me, here's my CV I also offer virtual tutoring for coding students."]])
 
 (defn layout
-  [realized-site {:keys [title category] :as page} & content]
+  [{:keys [title category] :as page} & content]
   (-> (html
         {:lang "en"}
         [:head [:title (if title (str title " | stel.codes") "stel.codes")] [:meta {:charset "utf-8"}]
@@ -94,17 +94,17 @@
          (when (:prod config)
            [:script
             {:src "https://plausible.io/js/plausible.js", :data-domain "stel.codes", :defer "defer", :async "async"}])]
-        [:body (header realized-site) [:main {:class (name category)} content] (footer)])
+        [:body (header page) [:main {:class (name category)} content] (footer)])
       (str)))
 
 (defn render-generic
-  [realized-site {:keys [repo prod source category title subtitle tags header-image render-resource] :as page}]
+  [{:keys [repo prod source category title subtitle tags header-image render-resource] :as page}]
   (layout page
           (welcome-section)
-          (window (state/kebab-case->lower-case (name category))
+          (window (util/kebab-case->lower-case (name category))
                   [:article (when header-image (image header-image)) [:h1 title]
                    (when subtitle [:p.subtitle subtitle])
-                   (when (not-empty tags) (tag-group realized-site tags))
+                   (when (not-empty tags) (tag-group page tags))
                    (when (or repo prod source)
                      [:div.top-links (when repo [:span "🧙 " [:a {:href repo} "Open Source Code Repo"]])
                       (when prod [:span "🌙 " [:a {:href prod} "Live App Demo"]])
@@ -113,32 +113,30 @@
                    [:div.circles (take 3 (repeat (raw (slurp "resources/svg/circle.svg"))))]])))
 
 (defn render-generic-index
-  [realized-site {:keys [title indexed-articles] :as page}]
+  [{:keys [title indexed-articles] :as page}]
   (layout
-   realized-site
    page
    (list (welcome-section)
          (window title
                  (unordered-list
-                  (map (partial window-list-item realized-site) indexed-articles))))))
+                  (map window-list-item indexed-articles))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Rendering multimethod declarations
 
 (defmulti render :category)
 
-(defmethod render :default [realized-site page] (render-generic realized-site page))
-
-(defmethod render :index [realized-site page] (render-generic-index realized-site page))
+(defmethod render :index [page] (render-generic-index page))
 
 (defmethod render :home
-  [realized-site page]
+  [page]
   (layout
-   realized-site
    page
    (list (welcome-section)
-         (home-content-window realized-site :coding-projects)
-         (home-content-window realized-site :educational-media)
-         (home-content-window realized-site :blog-posts))))
+         (home-content-window page :coding-projects)
+         (home-content-window page :educational-media)
+         (home-content-window page :blog-posts))))
 
-(defmethod render :404 [realized-site page] (layout realized-site page [:h1 "404 ;-;"]))
+(defmethod render :404 [page] (layout page [:h1 "404 ;-;"]))
+
+(defmethod render :default [page] (render-generic page))
