3cbdf1a073af6d431e9fc2fbfa1f36e12b465505
Stel Abrego <stel@stel.codes>
2022-02-15 13:00:25 -0800

    Restore dev namespace with simple http app for developement

--- dev/user.clj
+++ dev/user.clj
@@ -1,26 +1,20 @@
 (ns user
-  ;; (:require [clojure.repl :refer [doc find-doc apropos source]]
-  ;;           [ring.middleware.resource :refer [wrap-resource]]
-  ;;           [stasis.core :as stasis]
-  ;;           [ring.middleware.content-type :refer [wrap-content-type]]
-  ;;           [codes.stel.dev-blog.generator :as generator]
-  ;;           [codes.stel.dev-blog.state :as state]
-  ;;           [org.httpkit.server :refer [run-server]]
-  ;;           [taoensso.timbre :as timbre :refer [info]]
-  ;;           [codes.stel.dev-blog.config :refer [config]])
+  (:require [clojure.repl :as repl]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [stasis.core :as stasis]
+            [ring.middleware.content-type :refer [wrap-content-type]]
+            [codes.stel.dev-blog.generator :as generator]
+            [org.httpkit.server :refer [run-server]]
+            [taoensso.timbre :as log]))
 
-  )
+(defn dev-app
+  []
+  (-> (stasis/serve-pages #(generator/generate-page-index))
+      (wrap-resource "public")
+      wrap-content-type))
 
-;; (defn dev-app
-;;   []
-;;   (-> (stasis/serve-pages #(generator/generate-index (state/get-preview-pages)))
-;;       (wrap-resource "public")
-;;       wrap-content-type))
-;;
-;; (defn start
-;;   []
-;;   (let [dev-config (:dev config)
-;;         dev-port (:port dev-config)]
-;;     (info "Starting dev server on port " dev-port)
-;;     (run-server (dev-app) dev-config)))
-;;
+(defn start
+  []
+  (let [port 5775]
+    (log/info "✨ Starting dev server on port" port)
+    (run-server (dev-app) {:port port})))
