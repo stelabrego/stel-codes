b165a8b0e30cde3cd7d074f65e5c7c8f08889b5b
Stel Abrego <stel@stel.codes>
2022-02-23 18:52:25 -0800

    Add inspect function to user ns

--- dev/user.clj
+++ dev/user.clj
@@ -1,17 +1,22 @@
 (ns user
-  (:require [clojure.repl :as repl]
+  (:require [clojure.pprint :refer [pprint]]
+            [clojure.repl :as repl]
             [codes.stel.dev-blog.views :as views]
-            [codes.stel.nuzzle.core :as nuzzle]))
+            [codes.stel.nuzzle.api :as nuzzle]))
 
 (def config {:site-config "edn/site.edn"
-             :remove-drafts? true
+             :remove-drafts? false
              :static-dir "public"
              :render-fn views/render
              :target-dir "dist"})
 
-(defn export []
-  (nuzzle/export config))
-
 (defn start []
-  (nuzzle/start-server (assoc config :remove-drafts? false)))
+  (nuzzle/start-server config))
+
+(defn inspect []
+  (pprint (nuzzle/inspect config)))
+
+(defn export []
+  (nuzzle/export (assoc config :remove-drafts? true)))
+
 
