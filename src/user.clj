f64cbbd87a9ef5f57fa5eb554165809c629ee5e7
Stel Abrego <stel@stel.codes>
2022-04-22 23:25:28 -0700

    Update to Nuzzle v0.3.0
    Switch over to nuzzle.edn config format

--- src/user.clj
+++ src/user.clj
@@ -1,24 +1,13 @@
 (ns user
   (:require [clojure.pprint :refer [pprint]]
             [clojure.repl :as repl]
-            [views :as views]
             [nuzzle.api :as nuzzle]))
 
-(def config {:site-data "edn/site.edn"
-             :static-dir "static"
-             :render-webpage views/render-webpage
-             :chroma-style "dracula"
-             :rss-opts
-             {:author "stel@stel.codes (Stel Abrego)"
-              :title "Stel Codes"
-              :description "Thoughts about programming and whatever else."
-              :link "https://stel.codes"}})
-
 (defn start []
-  (nuzzle/start-server config))
+  (nuzzle/start-server))
 
 (defn realize []
-  (pprint (nuzzle/realize config)))
+  (pprint (nuzzle/realize)))
 
 (defn export []
-  (nuzzle/export (assoc config :remove-drafts? true)))
+  (nuzzle/export :remove-drafts? true))
